116fa3af39b8055d28202b7af936d7d88d9b7118
coord.e <me@coord-e.com>
2018-08-17 00:19:23 +0900

    Add: Add a simple client example

---
+++ examples/client/client.rs
@@ -0,0 +1,32 @@
+// Copyright 2017 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+extern crate grpcio;
+extern crate rami;
+
+use std::sync::Arc;
+
+use grpcio::{ChannelBuilder, EnvBuilder};
+use rami::protos::hello::HelloRequest;
+use rami::protos::hello_grpc::GreeterClient;
+
+fn main() {
+    let env = Arc::new(EnvBuilder::new().build());
+    let ch = ChannelBuilder::new(env).connect("localhost:50051");
+    let client = GreeterClient::new(ch);
+
+    let mut req = HelloRequest::new();
+    req.set_name("world".to_owned());
+    let reply = client.say_hello(&req).expect("rpc");
+    println!("Greeter received: {}", reply.get_message());
+}
