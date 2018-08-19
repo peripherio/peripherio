8c5203bc267ab4588bd7747753d60f78b7e23603
coord.e <me@coord-e.com>
2018-08-19 19:09:50 +0900

    Add: Add a minimal proto file for rami

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
     let proto_root = "src/protos";
     println!("cargo:rerun-if-changed={}", proto_root);
     protoc_grpcio::compile_grpc_protos(
-        &["example/hello.proto"],
+        &["main.proto"],
         &[proto_root],
         &proto_root
     ).expect("Failed to compile gRPC definitions!");
