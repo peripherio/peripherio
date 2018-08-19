2f062c56dcec533c8c03ca91c009a8e1bb82e753
coord.e <me@coord-e.com>
2018-08-19 19:31:09 +0900

    Add: impl FromStr on Category

--- src/device/category.rs
+++ src/device/category.rs
@@ -1,4 +1,17 @@
+use failure::Error;
+
+use std::str::FromStr;
+
 pub struct Category {
     name: String,
 }
 
+impl FromStr for Category {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Category {
+            name: s.to_owned()
+        })
+    }
+}
