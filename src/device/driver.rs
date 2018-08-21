3eea488e32c2111d99f6d66dd6c57d4399d07064
coord.e <me@coord-e.com>
2018-08-21 22:03:41 +0900

    Change: Replace driver with loader

--- src/device/driver.rs
+++ src/device/driver.rs
@@ -1,10 +1,68 @@
 use device::category::Category;
-use device::io::Interface;
+use device::libloading::{Library, Symbol};
+use resolve::resolve;
 
-trait Driver {
-    type Intf: Interface;
+use serde_yaml;
+use failure::Error;
 
-    fn name(&self) -> String;
-    fn category(&self) -> Category;
-    fn interface(&self) -> Self::Intf;
+use std::path::{Path, PathBuf};
+use std::fs::File;
+use std::io::Read;
+
+pub struct Driver {
+    path: PathBuf,
+    name: String,
+    version: String,
+    author: Option<String>,
+    category: Vec<Category>,
+    driver: Library
+}
+
+const COMMON_SYMBOLS: [&str; 2] = ["init", "detect"];
+
+#[derive(Deserialize)]
+struct LibMetaData {
+    name: String,
+    version: String,
+    author: Option<String>,
+    category: Vec<String>,
+    driver: Option<String>
+}
+
+impl Driver {
+    pub fn resolve(name: &str) -> Result<PathBuf, Error> {
+        resolve(name, "RAMI_PKG_PATH", "rami.yml")
+    }
+
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
+        let mut file = File::open(&path.as_ref().join("rami.yml"))?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        let metadata: LibMetaData = serde_yaml::from_str(&contents)?;
+
+        let driver_file = metadata.driver.unwrap_or(format!("{}.so", metadata.name));
+        let category = metadata.category.iter().map(|c| c.parse()).collect::<Result<Vec<_>, _>>()?;
+        Ok(Driver {
+            path: path.as_ref().to_path_buf(),
+            driver: Library::new(path.as_ref().join(driver_file))?,
+            name: metadata.name,
+            author: metadata.author,
+            version: metadata.version,
+            category
+        })
+    }
+
+    pub fn validate(&self) -> bool {
+        self.category.iter().flat_map(|ctg| ctg.required_symbols().iter()).map(AsRef::as_ref)
+            .chain(COMMON_SYMBOLS.into_iter().map(|e|*e))
+            .all(|sym| unsafe { self.get::<fn(u32) -> u32>(sym) }.is_ok())
+    }
+
+    pub fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    pub unsafe fn get<'lib, T: 'lib>(&'lib self, name: &str) -> Result<Symbol<'lib, T>, Error> {
+        self.driver.get(name.as_bytes()).map_err(|e| e.into())
+    }
 }
--- src/device/mod.rs
+++ src/device/mod.rs
@@ -1,6 +1,5 @@
 pub mod driver;
 pub mod io;
 pub mod category;
-pub mod loader;
 
 extern crate libloading;
