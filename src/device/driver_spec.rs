12011126acea81fc1137bb621365389e51a93996
coord.e <me@coord-e.com>
2018-08-23 15:59:03 +0900

    Add: Impl a method to check if the driver is conforming to the spec

--- src/device/driver_spec.rs
+++ src/device/driver_spec.rs
@@ -1,3 +1,5 @@
+use device::driver::Driver;
+
 pub struct DriverSpec {
     vendor: Option<String>,
     category: Option<String>,
@@ -7,7 +9,13 @@ pub struct DriverSpec {
 impl DriverSpec {
     pub fn new(vendor: Option<String>, category: Option<String>, name: Option<String>) -> Self {
         DriverSpec {
-            vendor, category. name
+            vendor, category, name
         }
+    }
+
+    pub fn is_conforming(&self, driver: &Driver) -> bool {
+        self.name.as_ref().map(|n| n == driver.name()).unwrap_or(true) &&
+        self.category.as_ref().map(|n| driver.category().into_iter().map(|c| c.name()).collect::<Vec<_>>().contains(&n)).unwrap_or(true) &&
+        self.vendor.as_ref().map(|n| Some(n) == driver.vendor().as_ref()).unwrap_or(true)
     }
 }
