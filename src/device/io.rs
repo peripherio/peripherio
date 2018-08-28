29bc34778585e396461f202aff04eae914279218
coord.e <me@coord-e.com>
2018-08-29 02:41:27 +0900

    Delete: Delete unused io module

--- src/device/io.rs
+++
@@ -1,5 +0,0 @@
-pub trait Interface {
-    fn name(&self) -> String;
-    fn speed(&self) -> u32;
-    fn set_speed(&self);
-}
--- src/device/mod.rs
+++ src/device/mod.rs
@@ -3,6 +3,5 @@ pub mod device;
 pub mod driver;
 pub mod driver_manager;
 pub mod driver_spec;
-pub mod io;
 
 extern crate libloading;
