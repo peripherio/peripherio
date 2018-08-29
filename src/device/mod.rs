cdf211a135cd2def9bf50523a33db06aaa335969
coord.e <me@coord-e.com>
2018-08-30 01:19:22 +0900

    Change: Move driver* to src/driver, and move category to src/

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -1,7 +1,1 @@
-pub mod category;
 pub mod device;
-pub mod driver;
-pub mod driver_manager;
-pub mod driver_spec;
-
-extern crate libloading;
---
+++ src/driver/mod.rs
@@ -0,0 +1,5 @@
+pub mod driver;
+pub mod driver_manager;
+pub mod driver_spec;
+
+extern crate libloading;
