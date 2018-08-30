b314a9388805c3c32595f39174ab00946077da7d
coord.e <me@coord-e.com>
2018-08-30 16:16:37 +0900

    [WIP] Add: Add an exmaple driver for mpu6050

---
+++ examples/driver/mpu6050/mpu6050.c
@@ -0,0 +1,22 @@
+#include "rami.gen.h"
+
+get_gyro_returns* get_gyro(get_gyro_args* args, Config* conf) {
+  /* Your Implementation! */
+}
+
+get_accel_returns* get_accel(get_accel_args* args, Config* conf) {
+  /* Your Implementation! */
+}
+
+get_temperature_returns* get_temperature(get_temperature_args* args, Config* conf) {
+  /* Your Implementation! */
+}
+
+void init() {
+  /* Your Implementation! */
+}
+
+Config** detect(Config* conf, size_t* size) {
+  /* Your Implementation! */
+}
+
