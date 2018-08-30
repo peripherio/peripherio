618de717af21016a000ac1cc8b8ea683b60f1aeb
coord.e <me@coord-e.com>
2018-08-30 15:01:21 +0900

    Change: Use BufWriter and write to file

--- src/codegen.rs
+++ src/codegen.rs
@@ -9,6 +9,8 @@ use rami::util;
 use rami::category::Signature;
 
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufWriter, Write};
 
 fn ctype(type_str: &str) -> String {
     match type_str {
@@ -31,21 +33,30 @@ fn merge_map(map1: &LinkedHashMap<String, serde_json::Value>, map2: &LinkedHashM
                         new_val
                     }
                     (Some(schema), None) => schema.clone(),
-                    _ => unreachable!()
+                    _ => unreachable!(),
                 },
             )
         }).collect()
 }
 
-
 fn main() {
     let drv = DriverData::new(".").unwrap();
 
-    println!("typedef struct Config_ {{");
-    for (key, val) in drv.schemas() {
-        println!("  {} {};", ctype(val["type"].as_str().unwrap()), key.replace(".", "_"));
-    }
-    println!("}} __attribute__((__packed__)) Config;");
+    let mut writer = BufWriter::new(File::create("rami.gen.h").unwrap());
+
+    let fields = drv.schemas().iter().fold(String::new(), |acc, (k, v)| {
+        format!(
+            "{}  {} {};\n",
+            acc,
+            ctype(v["type"].as_str().unwrap()),
+            k.replace(".", "_")
+        )
+    });
+    write!(
+        &mut writer,
+        "typedef struct Config_ {{\n{}}} __attribute__((__packed__)) Config;\n",
+        fields
+    );
 
     let mut merged_signs: HashMap<String, Signature> = HashMap::new();
     for ctg in drv.category() {
@@ -62,25 +73,58 @@ fn main() {
                 } else {
                     sign.returns.unwrap_or_default()
                 };
-                Signature { args:Some(args), returns: Some(returns) }
-            } else { sign };
+                Signature {
+                    args: Some(args),
+                    returns: Some(returns),
+                }
+            } else {
+                sign
+            };
             merged_signs.insert(name.clone(), gs);
         }
     }
 
     for (name, sign) in merged_signs {
-        println!("typedef struct {}_args_ {{", name);
-        for (key, val) in sign.args.clone().unwrap_or_default() {
-            println!("  {} {};", ctype(val["type"].as_str().unwrap()), key.replace(".", "_"));
-        }
-        println!("}} __attribute__((__packed__)) {}_args;", name);
+        let arg_fields =
+            sign.args
+                .clone()
+                .unwrap_or_default()
+                .iter()
+                .fold(String::new(), |acc, (key, val)| {
+                    format!(
+                        "{}  {} {};\n",
+                        acc,
+                        ctype(val["type"].as_str().unwrap()),
+                        key.replace(".", "_")
+                    )
+                });
+        write!(
+            &mut writer,
+            "typedef struct {0}_args_ {{\n{1}}} __attribute__((__packed__)) {0}_args;\n",
+            name, arg_fields
+        );
 
-        println!("typedef struct {}_returns_ {{", name);
-        for (key, val) in sign.returns.clone().unwrap_or_default() {
-            println!("  {} {};", ctype(val["type"].as_str().unwrap()), key.replace(".", "_"));
-        }
-        println!("}} __attribute__((__packed__)) {}_returns;", name);
-        println!("{0}_returns* {0}({0}* args, Config* conf);", name)
+        let return_fields = sign.returns.clone().unwrap_or_default().iter().fold(
+            String::new(),
+            |acc, (key, val)| {
+                format!(
+                    "{}  {} {};\n",
+                    acc,
+                    ctype(val["type"].as_str().unwrap()),
+                    key.replace(".", "_")
+                )
+            },
+        );
+        write!(
+            &mut writer,
+            "typedef struct {0}_returns_ {{\n{1}}} __attribute__((__packed__)) {0}_returns;\n",
+            name, return_fields
+        );
+
+        write!(
+            &mut writer,
+            "{0}_returns* {0}({0}_args* args, Config* conf);\n",
+            name
+        );
     }
 }
-
