fbd0961f2d3fc6a05a65838831464e77d11f97a1
coord.e <me@coord-e.com>
2018-08-31 01:38:58 +0900

    Add: Add a shallow library implementation

--- pyrami/rami.py
+++ pyrami/rami.py
@@ -4,13 +4,24 @@ import msgpack
 import main_pb2
 import main_pb2_grpc
 
+channel = grpc.insecure_channel('localhost:50051')
+stub = main_pb2_grpc.RamiStub(channel)
 
-def run():
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = main_pb2_grpc.RamiStub(channel)
-        response = stub.List(main_pb2.Config(config=[main_pb2.Config.Pair(key="if.type", value=msgpack.packb("i2c"))]))
-    print("Greeter client received: " + str(response.results))
+def convconf(conf):
+    return (main_pb2.Config.Pair(key=k, value=msgpack.packb(v)) for (k,v) in conf.items())
 
+def find_device(category, config={}):
+    p_spec = main_pb2.DriverSpecification(category=category)
+    p_conf = main_pb2.Config(config=list(convconf(config)))
+    response = stub.Find(main_pb2.FindRequest(config=p_conf, spec=p_spec))
+    return [Device(r.id) for r in response.results]
 
-if __name__ == '__main__':
-    run()
+class Device(object):
+    def __init__(self, device_id):
+        self.device_id = device_id
+
+    def __getattr__(self, name):
+        def __internal(args):
+            ret = stub.Dispatch(main_pb2.DispatchRequest(device=self.device_id, command=name, args=msgpack.packb(args)))
+            return msgpack.unpackb(ret.rets)
+        return __internal
