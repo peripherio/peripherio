0e0b79c1ff329123e2b17bcafa7aafdf5007af4a
coord.e <me@coord-e.com>
2018-09-01 05:37:09 +0900

    Change: Omit address in example

--- pyrami/examples/simple_get_gyro.py
+++ pyrami/examples/simple_get_gyro.py
@@ -1,7 +1,7 @@
 from rami import connect
 import time
 
-with connect('192.168.2.117:50051') as conn:
+with connect() as conn:
     device = conn.find_device('gyro', {'if.type': 'i2c'})[0]
     while True:
         print(device.get_gyro())
