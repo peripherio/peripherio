417ef7ea172516a3c646f222fd5900fdef2c0657
coord.e <me@coord-e.com>
2018-09-01 05:32:54 +0900

    Add: Add setup.py

---
+++ pyrami/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rami',
+        version='0.0.1',
+        description='Abstracted peripheral interface access',
+        author='coord.e',
+        author_email='me@coord-e.com',
+        url='https://github.com/coord-e/rami',
+        packages=find_packages(),
+        )
