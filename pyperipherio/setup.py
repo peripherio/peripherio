b4620a5e7c7fb6551b50c7d4ee8cc7a17276fb15
coord.e <me@coord-e.com>
2018-09-30 21:09:11 +0900

    Add: Add post-install process

--- pyperipherio/setup.py
+++ pyperipherio/setup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools.command.install import install
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+
 
 setup(name='peripherio',
         version='0.0.1',
@@ -10,4 +16,7 @@ setup(name='peripherio',
         url='https://github.com/peripherio/peripherio',
         install_requires=['grpcio-tools=1.14.2', 'grpcio>=1.14.2', 'msgpack>=0.5.6'],
         packages=find_packages(),
+        cmdclass={
+            'install': PostInstallCommand,
+        },
         )
